43b860af1de66bfc002c2499213d2d767213e60e
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-17 20:03:35 -0700

    Update exercise.py

--- assignments/01DiagonalRect/src/exercise.py
+++ assignments/01DiagonalRect/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
-    pass
+    #escribe tu código abajo de esta línea
+    altura=float(input())
+    angulo=int(input()
+    radianes=math.radians(angulo)
+    largo=round(altura/math.sin(radianes))
+    print(f"{largo}")
 
 
 
