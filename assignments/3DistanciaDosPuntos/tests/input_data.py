41427a0e5c03b45790f22c1194effa07b554c794
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-17 19:17:14 -0700

    Update input_data.py

--- assignments/3DistanciaDosPuntos/tests/input_data.py
+++ assignments/3DistanciaDosPuntos/tests/input_data.py
@@ -7,7 +7,7 @@ input_values = [
         # Inputs
         ["2", "-4", "5", "3"],
         # Outputs
-        ["x1= ", "y1= ", "x2= ", "y2= ","distancia= 7.62"],
+        ["", "", "", "","7.62"],
         # Error message
         "Revisa los tipos de dato de tus variables. Revisa el operador y el llamado a la libreria adecuada. Cuida los textos para la captura de las entradas y salida"
     ),
@@ -15,7 +15,7 @@ input_values = [
        # Inputs
         ["5", "10" ,"-3", "-4"],
         # Outputs
-        ["x1= ", "y1= ", "x2= ", "y2= ","distancia= 16.12"],
+        ["", "", "", "","16.12"],
         # Error message
         "Revisa los tipos de dato de tus variables. Revisa el operador y el llamado a la libreria adecuada. Cuida los textos para la captura de las entradas y salida"
     )]
